jpallister/stm32f4-energy-monitor

commit 36a8870e46025a5c839c2d442f1087a7956cb96c

Files are opened and closed with start and stop
A DataProcessor now only lasts as long as results are being recorded.
An alternate output file an be specified with the start command.

host/dataprocessor.cpp
@@ -26,6 +26,7 @@ DataProcessor::DataProcessor(boost::mutex *m, std::queue<boost::shared_array<uns
     gain = 50.0;
     referenceVoltage = 3.0;
     /* Don't set output. This should be done with openOutput. */
+    output = NULL;
 }
 
 DataProcessor::~DataProcessor()
@@ -136,12 +137,18 @@ void DataProcessor::processData()
             b2 |= (data[i]&0xF0) << 4;
 
             power = convertToPower(b1);
-            fprintf(output, "%f %lu\n", power, cur_time);
+            if (DataProcessor::openedFile())
+            {
+                fprintf(output, "%f %lu\n", power, cur_time);
+            }
             addDataItem(power, cur_time);
             cur_time += rate;
 
             power = convertToPower(b2);
-            fprintf(output, "%f %lu\n", power, cur_time);
+            if (DataProcessor::openedFile())
+            {
+                fprintf(output, "%f %lu\n", power, cur_time);
+            }
             addDataItem(power, cur_time);
             cur_time += rate;
         }
@@ -182,6 +189,11 @@ float DataProcessor::convertToPower(float v)
 
 int DataProcessor::closeOutput()
 {
+    if (status == 1)
+    {
+        return 1;
+    }
+
     int to_return = 0;
     if (output)
     {
@@ -211,4 +223,9 @@ int DataProcessor::openOutput(std::string output_loc)
     }
 
     return to_return;
+}
+
+int DataProcessor::openedFile()
+{
+    return (output != NULL);
 }

host/dataprocessor.h
@@ -31,6 +31,7 @@ public:
     int closeOutput();
     int openOutput();
     int openOutput(std::string output_loc);
+    int openedFile();
 private:
     boost::mutex *mQueue;
     std::queue<boost::shared_array<unsigned char> > *dQueue;

host/host_receiver.cpp
@@ -45,7 +45,10 @@ bool connected = false;
 void ctrlc_handler(int _)
 {
     liObj->endSignal();
-    dpObj->endSignal();
+    if (dpObj)
+    {
+        dpObj->endSignal();
+    }
     running = false;
 }
 
@@ -103,6 +106,7 @@ void processCommand(string input)
             thread2.join();
             delete liObj;
             delete dpObj;
+            dpObj = NULL;
         }
 
         dQueue.reset(new queue<shared_array<unsigned char> >());
@@ -110,7 +114,6 @@ void processCommand(string input)
         dpObj = new DataProcessor(&bmutex, dQueue.get());
 
         thread1 = boost::thread(bind(&LibusbInterface::operator(),liObj));  // Bind prevents copying obj (need to keep ptr)
-        thread2 = boost::thread(bind(&DataProcessor::operator(),dpObj));
 
         connected = true;
         cout << "    Connected to device, serial: " << chosen_serial << endl;
@@ -234,11 +237,41 @@ void processCommand(string input)
     else if(args[0] == "start")
     {
         CHECK_CONNECTED();
+
+        /* Relies on us always using thread2.join() before dpObjs deletion */
+        if (thread2.joinable())
+        {
+            cout << "Already started!" << endl;
+            return;
+        }
+
+        if (args.size() == 2)
+        {
+            dpObj->openOutput(args[1]);
+        }
+        else
+        {
+            dpObj->openOutput();
+        }
+
+        if (!dpObj->openedFile())
+        {
+            cout << "An output file is not open ";
+            cout << "so no results will be recorded" << endl;
+        }
+
+        thread2 = boost::thread(bind(&DataProcessor::operator(),dpObj));
         liObj->sendCommand(LibusbInterface::START);
     }
     else if(args[0] == "stop")
     {
         CHECK_CONNECTED();
+
+        dpObj->endSignal();
+        thread2.join();
+        dpObj->closeOutput();
+        delete dpObj;
+        dpObj = new DataProcessor(&bmutex, dQueue.get());
         liObj->sendCommand(LibusbInterface::STOP);
     }
     else if(args[0] == "power")
@@ -284,7 +317,7 @@ void processCommand(string input)
         cout << "    setresistor RESISTOR        Set the value of the shunt resistor" << endl;
         cout << "    setrefvoltage REFVOLTAGE    Set the value of the ADC reference voltage" << endl;
         cout << "    setgain GAIN                Set the gain of the high side current amplifier" << endl;
-        cout << "    start                       Start energy measurement" << endl;
+        cout << "    start [FILE]                Start energy measurement, collecting results into FILE (output_results by default)" << endl;
         cout << "    stop                        Stop energy measurement" << endl;
         cout << "    trigger" << endl;
         cout << "             PIN                Trigger measurement on PIN (e.g PA0)" << endl;

commit f64009a80d98c9d27cefcdd69373789453b9508a

Fixed output file not changing with trigger commands
Issue started with commit 36a8870e46.
DataProcessor's output file was not changing with triggers from an external
board.
Command has now been implemented by sending commands through the data queue
between LibusbInterface and DataProcessor, not just data.
DataProcessors also last the life of the program again.

host/dataprocessor.cpp
@@ -12,7 +12,7 @@ using namespace std;
 using namespace boost;
 using namespace boost::accumulators;
 
-DataProcessor::DataProcessor(boost::mutex *m, std::queue<boost::shared_array<unsigned char> > *d)
+DataProcessor::DataProcessor(boost::mutex *m, std::queue<DataSet> *d)
 {
     mQueue = m;
     dQueue = d;
@@ -114,43 +114,58 @@ void DataProcessor::processData()
     short rate = 0;
     float power;
 
-    for(i = 0; i < DATA_LEN; ++i, ++c)
+    if (data.type == COMMAND)
     {
-        if(i % 64 == 0)
+        switch (data.data[0])
         {
-
-            rate = data[i];
-            // printf("%d\n", rate);
-            c = 2;
+            case LibusbInterface::START:
+                this->openOutput();
+                break;
+            case LibusbInterface::STOP:
+                this->closeOutput();
+                break;
         }
-        else if(c % 3 == 0)
+    }
+    else
+    {
+        for(i = 0; i < DATA_LEN; ++i, ++c)
         {
-            b1 = data[i];
-        }
-        else if(c % 3 == 1)
-        {
-            b2 = data[i];
-        }
-        else
-        {
-            b1 |= (data[i]&0x0F) << 8;
-            b2 |= (data[i]&0xF0) << 4;
-
-            power = convertToPower(b1);
-            if (DataProcessor::openedFile())
+            if(i % 64 == 0)
             {
-                fprintf(output, "%f %lu\n", power, cur_time);
-            }
-            addDataItem(power, cur_time);
-            cur_time += rate;
 
-            power = convertToPower(b2);
-            if (DataProcessor::openedFile())
-            {
-                fprintf(output, "%f %lu\n", power, cur_time);
+                rate = data.data[i];
+                // printf("%d\n", rate);
+                c = 2;
             }
-            addDataItem(power, cur_time);
-            cur_time += rate;
+            else if(c % 3 == 0)
+            {
+                b1 = data.data[i];
+            }
+            else if(c % 3 == 1)
+            {
+                b2 = data.data[i];
+            }
+            else
+            {
+                b1 |= (data.data[i]&0x0F) << 8;
+                b2 |= (data.data[i]&0xF0) << 4;
+
+                power = convertToPower(b1);
+                if (DataProcessor::openedFile())
+                {
+                    fprintf(output, "%f %lu\n", power, cur_time);
+                }
+                addDataItem(power, cur_time);
+                cur_time += rate;
+
+                power = convertToPower(b2);
+                if (DataProcessor::openedFile())
+                {
+                    fprintf(output, "%f %lu\n", power, cur_time);
+                }
+                addDataItem(power, cur_time);
+                cur_time += rate;
+            }
         }
     }
 }

host/dataprocessor.h
@@ -11,6 +11,8 @@
 #include <boost/accumulators/statistics/min.hpp>
 #include <boost/accumulators/statistics/max.hpp>
 
+#include "libusbinterface.h"
+
 #define TIMER_SECOND_TICKS      84000000
 
 namespace _ba =  boost::accumulators;
@@ -18,7 +20,7 @@ namespace _ba =  boost::accumulators;
 class DataProcessor
 {
 public:
-    DataProcessor(boost::mutex *, std::queue<boost::shared_array<unsigned char> > *);
+    DataProcessor(boost::mutex *, std::queue<DataSet> *);
     ~DataProcessor();
     void operator()();
     void endSignal();
@@ -39,8 +41,8 @@ private:
     };
 
     boost::mutex *mQueue;
-    std::queue<boost::shared_array<unsigned char> > *dQueue;
-    boost::shared_array<unsigned char> data;
+    std::queue<DataSet> *dQueue;
+    DataSet data;
 
     int status;
     bool isEmpty;

host/host_receiver.cpp
@@ -34,7 +34,7 @@ using namespace std;
 using namespace boost;
 
 boost::mutex bmutex;
-scoped_ptr<queue<shared_array<unsigned char> > > dQueue;
+scoped_ptr<queue<DataSet> > dQueue;
 LibusbInterface *liObj;
 DataProcessor *dpObj;
 boost::thread thread1, thread2;
@@ -46,10 +46,7 @@ bool connected = false;
 void ctrlc_handler(int _)
 {
     liObj->endSignal();
-    if (dpObj)
-    {
-        dpObj->endSignal();
-    }
+    dpObj->endSignal();
     running = false;
 }
 
@@ -104,11 +101,12 @@ void cmd_connect_to(string connect_to)
         dpObj = NULL;
     }
 
-    dQueue.reset(new queue<shared_array<unsigned char> >());
+    dQueue.reset(new queue<DataSet>());
     liObj = new LibusbInterface(&bmutex, dQueue.get(), 0xF539, 0xF539, chosen_serial);
     dpObj = new DataProcessor(&bmutex, dQueue.get());
 
     thread1 = boost::thread(bind(&LibusbInterface::operator(),liObj));  // Bind prevents copying obj (need to keep ptr)
+    thread2 = boost::thread(bind(&DataProcessor::operator(),dpObj));
 
     connected = true;
     cout << "    Connected to device, serial: " << chosen_serial << endl;
@@ -226,36 +224,15 @@ void cmd_leds()
 
 void cmd_start()
 {
-    cmd_start_with_file("");
+    CHECK_CONNECTED();
+    liObj->sendCommand(LibusbInterface::START);
 }
 
 void cmd_start_with_file(string output_file)
 {
     CHECK_CONNECTED();
 
-    /* Relies on us always using thread2.join() before dpObjs deletion */
-    if (thread2.joinable())
-    {
-        cout << "Already started!" << endl;
-        return;
-    }
-
-    if (output_file != "")
-    {
-        dpObj->openOutput(output_file);
-    }
-    else
-    {
-        dpObj->openOutput();
-    }
-
-    if (!dpObj->openedFile())
-    {
-        cout << "An output file is not open ";
-        cout << "so no results will be recorded" << endl;
-    }
-
-    thread2 = boost::thread(bind(&DataProcessor::operator(),dpObj));
+    dpObj->openOutput(output_file);
     liObj->sendCommand(LibusbInterface::START);
 }
 
@@ -263,11 +240,6 @@ void cmd_stop()
 {
     CHECK_CONNECTED();
 
-    dpObj->endSignal();
-    thread2.join();
-    dpObj->closeOutput();
-    delete dpObj;
-    dpObj = new DataProcessor(&bmutex, dQueue.get());
     liObj->sendCommand(LibusbInterface::STOP);
 }
 

host/libusbinterface.cpp
@@ -11,7 +11,7 @@ using namespace boost;
 // using namespace boost::bind;
 using namespace std;
 
-LibusbInterface::LibusbInterface(boost::mutex *m, queue<shared_array<unsigned char> > *d,
+LibusbInterface::LibusbInterface(boost::mutex *m, queue<DataSet> *d,
     unsigned idVendor, unsigned idProduct, string serialId)
 {
     mQueue = m;
@@ -243,13 +243,12 @@ void LibusbInterface::close_device()
     }
 }
 
-void LibusbInterface::send_data(shared_array<unsigned char> data)
+void LibusbInterface::send_data(DataSet data)
 {
     boost::mutex::scoped_lock lock(*mQueue);
 
     dQueue->push(data);
 }
-
 
 void LIBUSB_CALL LibusbInterface::transfer_callback(struct libusb_transfer *transfer)
 {
@@ -280,9 +279,12 @@ void LIBUSB_CALL LibusbInterface::transfer_callback(struct libusb_transfer *tran
     // printf("cb %d\n", _this->data_buf[0]);
     _this->total_len += DATA_LEN;
 
-    shared_array<unsigned char> data(new unsigned char[DATA_LEN]);
+    DataSet data;
+    shared_array<unsigned char> dataSet(new unsigned char[DATA_LEN]);
+    data.data = dataSet;
+    data.type = ENERGY_DATA;
 
-    memcpy(data.get(), _this->data_buf, DATA_LEN);
+    memcpy(data.data.get(), _this->data_buf, DATA_LEN);
     _this->send_data(data);
 
     if((r=libusb_submit_transfer(transfer)) < 0)
@@ -324,6 +326,7 @@ void LIBUSB_CALL LibusbInterface::interrupt_callback(struct libusb_transfer *tra
         mt_end_output();
         if(!_this->running)
         {
+            _this->sendCommand(START);
             libusb_submit_transfer(_this->energy_transfer);
         }
         _this->running = true;
@@ -335,6 +338,7 @@ void LIBUSB_CALL LibusbInterface::interrupt_callback(struct libusb_transfer *tra
         mt_end_output();
         if(_this->running)
         {
+            _this->sendCommand(STOP);
             libusb_cancel_transfer(_this->energy_transfer);
         }
         _this->running = false;
@@ -364,6 +368,13 @@ void LibusbInterface::sendCommand(CommandType cmd)
     boost::mutex::scoped_lock lock(cQueueMutex);
     LibusbInterface::CommandData cd = {cmd, ""};
     cQueue.push(cd);
+
+    DataSet data;
+    shared_array<unsigned char> dataSet(new unsigned char[1]);
+    data.data = dataSet;
+    data.type = COMMAND;
+    data.data[0] = cmd;
+    this->send_data(data);
 }
 
 void LibusbInterface::setSerial(string ser)

host/libusbinterface.h
@@ -9,10 +9,20 @@
 
 #define DATA_LEN    2048
 
+enum DataSetType {
+    ENERGY_DATA = 0,
+    COMMAND = 1
+};
+
+struct DataSet {
+    boost::shared_array<unsigned char> data;
+    DataSetType type;
+};
+
 class LibusbInterface
 {
 public:
-    LibusbInterface(boost::mutex *, std::queue<boost::shared_array<unsigned char> > *, unsigned idVendor, unsigned idProduct, std::string serialId);
+    LibusbInterface(boost::mutex *, std::queue<DataSet> *, unsigned idVendor, unsigned idProduct, std::string serialId);
     ~LibusbInterface();
     void operator()();
     void endSignal();
@@ -42,7 +52,7 @@ public:
 private:
     boost::mutex *mQueue;
     boost::mutex cQueueMutex;
-    std::queue<boost::shared_array<unsigned char> > *dQueue;
+    std::queue<DataSet> *dQueue;
 
     // Attributes to look for in the USB devices
     unsigned idProduct, idVendor;
@@ -57,7 +67,7 @@ private:
     void close_device();
 
     // Send data to the data processor thread
-    void send_data(boost::shared_array<unsigned char>);
+    void send_data(DataSet);
 
     int status;
     libusb_device_handle *devh;