f87c327ed3232f2241e7b512b4d283be4f29638c
James Pallister <james.pallister@bristol.ac.uk>
2014-02-11 16:40:38 +0000

    Added a cdc_acm interface to the firmware

--- src/firmware/energy_monitor.c
+++ src/firmware/energy_monitor.c
@@ -69,16 +69,116 @@ static const struct usb_interface_descriptor data_iface[] = {{
     .endpoint = data_endp,
 }};
 
+static const struct {
+    struct usb_cdc_header_descriptor header;
+    struct usb_cdc_call_management_descriptor call_mgmt;
+    struct usb_cdc_acm_descriptor acm;
+    struct usb_cdc_union_descriptor cdc_union;
+} __attribute__((packed)) cdcacm_functional_descriptors = {
+    .header = {
+        .bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
+        .bDescriptorType = CS_INTERFACE,
+        .bDescriptorSubtype = USB_CDC_TYPE_HEADER,
+        .bcdCDC = 0x0110,
+    },
+    .call_mgmt = {
+        .bFunctionLength =
+            sizeof(struct usb_cdc_call_management_descriptor),
+        .bDescriptorType = CS_INTERFACE,
+        .bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
+        .bmCapabilities = 0,
+        .bDataInterface = 2,
+    },
+    .acm = {
+        .bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
+        .bDescriptorType = CS_INTERFACE,
+        .bDescriptorSubtype = USB_CDC_TYPE_ACM,
+        .bmCapabilities = 0,
+    },
+    .cdc_union = {
+        .bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
+        .bDescriptorType = CS_INTERFACE,
+        .bDescriptorSubtype = USB_CDC_TYPE_UNION,
+        .bControlInterface = 1,
+        .bSubordinateInterface0 = 2,
+     }
+};
+
+
+static const struct usb_endpoint_descriptor cdc_comm_endp[] = {{
+    .bLength = USB_DT_ENDPOINT_SIZE,
+    .bDescriptorType = USB_DT_ENDPOINT,
+    .bEndpointAddress = 0x85,
+    .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+    .wMaxPacketSize = 16,
+    .bInterval = 255,
+}};
+
+static const struct usb_interface_descriptor cdc_comm_iface[] = {{
+    .bLength = USB_DT_INTERFACE_SIZE,
+    .bDescriptorType = USB_DT_INTERFACE,
+    .bInterfaceNumber = 1,
+    .bAlternateSetting = 0,
+    .bNumEndpoints = 1,
+    .bInterfaceClass = USB_CLASS_CDC,
+    .bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
+    .bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
+    .iInterface = 0,
+
+    .endpoint = cdc_comm_endp,
+
+    .extra = &cdcacm_functional_descriptors,
+    .extralen = sizeof(cdcacm_functional_descriptors)
+}};
+
+
+static const struct usb_endpoint_descriptor cdc_data_endp[] = {{
+    .bLength = USB_DT_ENDPOINT_SIZE,
+    .bDescriptorType = USB_DT_ENDPOINT,
+    .bEndpointAddress = 0x03,
+    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
+    .wMaxPacketSize = 64,
+    .bInterval = 1,
+}, {
+    .bLength = USB_DT_ENDPOINT_SIZE,
+    .bDescriptorType = USB_DT_ENDPOINT,
+    .bEndpointAddress = 0x84,
+    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
+    .wMaxPacketSize = 64,
+    .bInterval = 1,
+}};
+
+
+static const struct usb_interface_descriptor cdc_data_iface[] = {{
+    .bLength = USB_DT_INTERFACE_SIZE,
+    .bDescriptorType = USB_DT_INTERFACE,
+    .bInterfaceNumber = 2,
+    .bAlternateSetting = 0,
+    .bNumEndpoints = 2,
+    .bInterfaceClass = USB_CLASS_DATA,
+    .bInterfaceSubClass = 0,
+    .bInterfaceProtocol = 0,
+    .iInterface = 0,
+
+    .endpoint = cdc_data_endp,
+}};
+
 static const struct usb_interface ifaces[] = {{
     .num_altsetting = 1,
     .altsetting = data_iface,
+}, {
+    .num_altsetting = 1,
+    .altsetting = cdc_comm_iface,
+}, {
+    .num_altsetting = 1,
+    .altsetting = cdc_data_iface,
 }};
 
 static const struct usb_config_descriptor config = {
     .bLength = USB_DT_CONFIGURATION_SIZE,
     .bDescriptorType = USB_DT_CONFIGURATION,
     .wTotalLength = 0,
-    .bNumInterfaces = 1,
+    .bNumInterfaces = 3,
     .bConfigurationValue = 1,
     .iConfiguration = 0,
     .bmAttributes = 0x80,
@@ -262,6 +362,46 @@ void stop_measurement(int m_point)
         default:
             error_condition(); return;
     }
+}
+
+static int cdcacm_control_request(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
+        uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req))
+{
+    (void)complete;
+    (void)buf;
+    (void)usbd_dev;
+
+    switch (req->bRequest) {
+    case USB_CDC_REQ_SET_CONTROL_LINE_STATE: {
+        /*
+         * This Linux cdc_acm driver requires this to be implemented
+         * even though it's optional in the CDC spec, and we don't
+         * advertise it in the ACM functional descriptor.
+         */
+        return 1;
+        }
+    case USB_CDC_REQ_SET_LINE_CODING:
+        if (*len < sizeof(struct usb_cdc_line_coding))
+            return 0;
+
+        return 1;
+    }
+    return 0;
+}
+
+static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
+{
+    (void)ep;
+
+    char buf[64];
+    int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
+
+    if (len) {
+        while (usbd_ep_write_packet(usbd_dev, 0x82, buf, len) == 0)
+            ;
+    }
+
+    gpio_toggle(GPIOC, GPIO5);
 }
 
 static int usbdev_control_request(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
@@ -479,6 +619,18 @@ static void usbdev_set_config(usbd_device *usbd_dev, uint16_t wValue)
                 usbdev_control_request);
 
     usbd_register_reset_callback(usbd_dev, usb_reset_cb);
+
+
+    // usbd_ep_setup(usbd_dev, 0x03, USB_ENDPOINT_ATTR_BULK, 64, NULL);
+    usbd_ep_setup(usbd_dev, 0x03, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);
+    usbd_ep_setup(usbd_dev, 0x84, USB_ENDPOINT_ATTR_BULK, 64, NULL);
+    usbd_ep_setup(usbd_dev, 0x85, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+
+    usbd_register_control_callback(
+                usbd_dev,
+                USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
+                USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
+                cdcacm_control_request);
 }
 
 void timer_setup()
