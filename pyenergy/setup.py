db4b54884b45570c440bb71f021011a7d0d0163a
James Pallister <james.pallister@bristol.ac.uk>
2014-03-17 16:38:05 +0000

    Updated version to 0.1rc6

--- pyenergy/setup.py
+++ pyenergy/setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "pyenergy",
-    version = "0.1rc5",
+    version = "0.1rc6",
     author = "James Pallister",
     author_email = "james.pallister@bristol.ac.uk",
     description = ("An interface to the MAGEEC energy monitor boards"),
